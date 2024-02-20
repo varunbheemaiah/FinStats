ed19238af71339c1d35f945b90218d895e29c8b2
Varun Bheemaiah <varunbheemaiah9@gmail.com>
2024-02-21 02:00:29 +0530

    Added new icons and logo

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,24 +1,26 @@
 <template>
-    <v-container fluid class="full-height">
-      <v-row class="full-height" align="center" justify="center">
-        <v-col cols="12">
-          <h1 class="title">FinStats</h1>
-          <h2 class="subtitle">The one stop shop for all finance statistics</h2>
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
+  <v-container fluid class="full-height">
+    <v-row class="full-height" align="center" justify="center">
+      <v-col cols="12">
+        <h1 class="title">FinStats</h1>
+        <h2 class="subtitle">The one stop shop for all finance statistics</h2>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
   
-  <style scoped>
-  .full-height {
-    height: 100vh; /* Set height to full viewport height */
-  }
-  
-  .title {
-    font-size: 4rem; /* Adjust font size for title */
-  }
-  
-  .subtitle {
-    font-size: 2rem; /* Adjust font size for subtitle */
-  }
-  </style>
+<style scoped>
+.full-height {
+  height: 100vh;
+  /* Set height to full viewport height */
+}
+
+.title {
+  font-size: 4rem;
+  /* Adjust font size for title */
+}
+
+.subtitle {
+  font-size: 2rem;
+  /* Adjust font size for subtitle */
+}</style>
--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -1,6 +1,15 @@
 <template>
     <v-navigation-drawer app>
-        <v-list-item title="FinStats"></v-list-item>
+        <v-list-item>
+            <template #prepend>
+                <v-list-item-avatar class="square-avatar">
+                    <img src="../assets/images/stock-market-no-base.png" alt="FinStats Image">
+                </v-list-item-avatar>
+            </template>
+            <v-list-item-content>
+                <v-list-item-title>FinStats</v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>
         <v-divider></v-divider>
         <router-link to="/nasdaq-earnings">
             <v-list-item link title="NASDAQ Earnings Calendar"></v-list-item>
@@ -14,12 +23,18 @@
   
 <style scoped>
 .v-list-item:first-child {
-    margin: 0.75em 0em;
+    margin: 0.5em 0em;
 }
+
 a {
     text-decoration: none;
     color: white;
     opacity: 0.7;
+}
+
+.square-avatar img {
+    width: 36px;
+    /* border-radius: 0; */
 }
 </style>
   
